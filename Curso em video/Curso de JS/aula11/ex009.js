f1ee992b2bd9d3617d1d053d2110c128a0c853d0
Rychard <rychardsouzadev@gmail.com>
2023-06-09 00:51:47 -0300

    Atualização

--- Curso em video/Curso de JS/aula11/ex009.js
+++ Curso em video/Curso de JS/aula11/ex009.js
@@ -1,8 +1,14 @@
-var pais = 'EUA'
-console.log(`Vivendo em ${pais}`)
-if (pais == 'Brasil') {
-    console.log('Você é Brasileiro')
-}
-else {
-    console.log('Você é estrangeiro')
+function clicar() {
+    var p = document.getElementById('pais')
+    var valor = p.value;
+    var final = valor.toUpperCase()
+    var res = document.getElementById('res')
+    
+
+    if ( final == 'BRASIL' ) {
+        res.innerHTML += `<p>Sim, você é brasileiro</p>`
+    }
+    else {
+        res.innerHTML += `<p>Você é <strong>estrangeiro</strong></p>`
+    }
 }
