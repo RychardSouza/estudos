97a26e14436089bb30ffad2f66aec32d3da4146d
Rychard <rychardsouzadev@gmail.com>
2023-06-09 18:26:43 -0300

    Sistema de repetição

---
+++ Curso em video/Curso de JS/aula13/ambiente.js
@@ -0,0 +1,16 @@
+/* Forma prática */
+var c = 1
+ while (c <= 6) {
+    console.log('tudo bem')
+    c++
+ }
+
+/*
+Para mostrar 6 vezes "Tudo bem" no console de forma não prática
+
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem') */
